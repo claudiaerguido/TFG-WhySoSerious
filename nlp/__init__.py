57d37d13561642cee27ac12c77bae3d9ff50c0f2
Claudia Erguido <claudia.erguidoaguilar@usp.ceu.es>
2026-01-17 22:13:08 +0100

    feat(nlp): add modular nlp package and HF sentiment model

---
+++ nlp/__init__.py
@@ -0,0 +1,5 @@
+from .base import NLPModel
+from .model_sentiment_hf import SentimentHFModel
+# Los otros modelos se exportarán cuando estén implementados
+# from .model_zero_shot import ZeroShotModel
+# from .model_finetune import FineTunedModel
